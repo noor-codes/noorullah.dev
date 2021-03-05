e2644b4327bac8a1800856499e990d6a7491ad85
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-05 17:51:45 +0430

    xs breakpoint

--- pages/index.js
+++ pages/index.js
@@ -25,20 +25,26 @@ const Blog = () => (
     </Head>
 
     <div className="flex items-center pt-14">
-      <div className="h-1 bg-black w-32 mr-2">&nbsp;</div>
+      <div className="h-1 bg-black w-32 mr-2 xs:w-40">&nbsp;</div>
       <div className="font-sans font-medium leading-5">Blog</div>
     </div>
-    <main className="container px-2">
+    <main className="xs:px-4 mx-auto md:container lg:container">
       <div className="mt-8 mb-4 px-8">
-        <h1 className="font-semibold leading-tight text-2xl tracking-wide capitalize">
+        <h1 className="font-semibold leading-tight text-2xl tracking-wide capitalize xs:font-bold">
           Local Authentication Using Passport in Node.js
         </h1>
       </div>
       <div className="flex items-center font-sans ml-8 mr-10">
-        <img className="rounded-full h-14 w-14 mr-3" src="/img/profile.jpg" alt="profile" />
+        <img
+          className="rounded-full h-14 w-14 mr-3 xs:h-16 xs:w-16"
+          src="/img/profile.jpg"
+          alt="Noorullah Ahmadzai"
+        />
         <div>
-          <h2 className="text-base font-semibold text-gray-600">Noorullah Ahmadzai</h2>
-          <p className="flex flex-wrap text-xs mt-0.5 text-gray-500">
+          <h2 className="text-base font-semibold text-gray-600 tacking-wide xs:text-lg">
+            Noorullah Ahmadzai
+          </h2>
+          <p className="flex flex-wrap text-xs mt-0.5 text-gray-500 xs:text-sm">
             <p>16 mins read&nbsp; - &nbsp; </p> <p> 576 views - &nbsp;</p>
             <p>May 3rd, 2020</p>
           </p>
@@ -73,7 +79,7 @@ const Blog = () => (
         </div>
       </div>
       <div className="px-8">
-        <p className="text-sm mt-6 text-gray-700 leading-6 ">
+        <p className="text-sm mt-6 text-gray-700 leading-6 xs:text-base xs:tracking-tight">
           For the better part of the last year, I have been investing heavily in front-end
           development and design. When I started my new role, I identified a need for a component
           library and created it. Since then, I have learned a lot about style guides, design
@@ -81,9 +87,9 @@ const Blog = () => (
           everything I have learned in the past year.
         </p>
       </div>
-      <h2 className="ml-8 mt-6 font-semibold mb-1">Why Should You Care?</h2>
+      <h2 className="ml-8 mt-6 font-semibold mb-1 xs:text-lg">Why Should You Care?</h2>
       <div className="px-8">
-        <p className="text-sm text-gray-700 leading-6">
+        <p className="text-sm text-gray-700 leading-6 xs:text-base xs:tracking-tight">
           Every website starts simple. There is maybe one page with a few distinct pieces. It has
           modest intentions.
         </p>
@@ -92,22 +98,22 @@ const Blog = () => (
         <img className="w-full" src="/img/article.png" alt="article" />
       </div>
 
-      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 ">
+      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 xs:text-base xs:tracking-tight">
         Then, slowly, it begins to scale.
       </p>
 
-      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 ">
+      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 xs:text-base xs:tracking-tight">
         More pages are added and new features are built. There might even be multiple teams devoted
         to specific sections of the site. You could be developing for mobile, too.
       </p>
 
-      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 ">
+      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 xs:text-base xs:tracking-tight">
         You start to notice the buttons in one part of the site are slightly different than
         everywhere else. One team decides to build a feature that another team has (unknowingly)
         already completed. Communication breakdowns happen. Consistency is lost.
       </p>
 
-      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 ">
+      <p className="px-8 text-sm mt-2 text-gray-700 leading-6 xs:text-base xs:tracking-tight">
         Is this a preventable problem? Absolutely. Yet, why does it happen over and over again?
         Unless you think about your design and development process upfront, you will run into issues
         later as you scale.
@@ -122,7 +128,7 @@ const Blog = () => (
         </div>
       </div>
 
-      <div className="flex flex-col pr-10 space-y-2">
+      <div className="flex flex-col pr-10 space-y-2 xs:flex-row xs:space-y-0 xs:space-x-3">
         <a href="#" rel="noreferrer" target="_blank">
           <TwitterBlack />
         </a>
@@ -137,11 +143,17 @@ const Blog = () => (
       </div>
     </div>
 
-    <div className="flex flex-col items-center font-sans ml-8 mr-8 mt-10 text-center">
-      <img className="rounded-full h-24 w-24 mr-3" src="/img/profile.jpg" alt="profile" />
+    <div className="flex flex-col items-center font-sans ml-8 mr-8 mt-10 text-center xs:mt-16">
+      <img
+        className="rounded-full h-24 w-24 xs:h-32 xs:w-32 mr-3"
+        src="/img/profile.jpg"
+        alt="profile"
+      />
       <div>
-        <h2 className="text-lg font-semibold text-gray-800 mt-3">About the Author</h2>
-        <p className="text-sm mt-1 text-gray-500 tracking-tight leading-5">
+        <h2 className="text-lg font-semibold text-gray-800 mt-3 xs:text-xl xs:mt-4">
+          About the Author
+        </h2>
+        <p className="text-sm mt-1 text-gray-500 tracking-tight leading-5 xs:text-base">
           My name is Noorullah Ahmadzi. A full stack web developer who specializes in React, NodeJs,
           MongoDB and JamStack. I&apos;m passinate about web development and technology.
         </p>

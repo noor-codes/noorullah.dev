59a164b5b669378ae50570745a033be6514e1468
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-08 18:07:25 +0430

    webpack file loader to use SVG as src for responsiveness and SVG Support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      test: /\.(png|jp(e*)g|svg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            context: 'public',
+          },
+        },
+      ],
     })
 
     return config

757398f753aa13b433691fb1d69fdc92286cc260
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-08 12:18:03 +0430

    Unique Key

--- components/blog/TagsCollection.jsx
+++ components/blog/TagsCollection.jsx
@@ -3,7 +3,10 @@ const TagsCollection = () => (
     <div className="flex flex-wrap gap-2 ml-8 mr-10 text-center">
       {['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'NodeJS', 'MongoDB', 'Firebase'].map(
         (name) => (
-          <div className="bg-gray-100 flex-1 text-gray-500 text-tiny  px-3 py-1.5 md:text-bigger rounded-sm">
+          <div
+            key={name}
+            className="bg-gray-100 flex-1 text-gray-500 text-tiny  px-3 py-1.5 md:text-bigger rounded-sm"
+          >
             {name}
           </div>
         )

06a8e9a0932cc0a5c43e5ffdcaf619a13f2a6aaa
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-08 23:40:31 +0430

    Responsive Sharing and Footer SVG

--- components/blog/Footer.jsx
+++ components/blog/Footer.jsx
@@ -3,35 +3,19 @@ const Footer = () => (
     <p className="text-white text-3xl mx-10 text-center xl:text-4xl xl:mb-2">Get in Touch</p>
     <div className="flex justify-center flex-row-reverse space-x-2 md:space-x-3 md:space-x-reverse space-x-reverse">
       <a href="https://github.com/Noorullah-Ahmadzai" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/white/github-icon.svg"
-          alt="GitHub"
-          className="h-8 w-8 xl:h-9 xl:w-9"
-        />
+        <img src="/icons/social/white/github-icon.svg" alt="GitHub" className="h-8 xl:h-9" />
       </a>
 
       <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/white/twitter-icon.svg"
-          alt="Twitter"
-          className="h-8 w-8 xl:h-9 xl:w-9"
-        />
+        <img src="/icons/social/white/twitter-icon.svg" alt="Twitter" className="h-8 xl:h-9" />
       </a>
 
       <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/white/instagram-icon.svg"
-          alt="Instagram"
-          className="h-8 w-8 xl:h-9 xl:w-9"
-        />
+        <img src="/icons/social/white/instagram-icon.svg" alt="Instagram" className="h-8 xl:h-9" />
       </a>
 
       <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/white/linkedin-icon.svg"
-          alt="LinkedIn"
-          className="h-8 w-8 xl:h-9 xl:w-9"
-        />
+        <img src="/icons/social/white/linkedin-icon.svg" alt="LinkedIn" className="h-8 xl:h-9" />
       </a>
     </div>
   </div>
--- components/blog/Sharing.jsx
+++ components/blog/Sharing.jsx
@@ -1,7 +1,7 @@
 const Sharing = () => (
   <div className="flex items-center justify-between mt-16 xl:pt-10">
     <div className="flex items-center">
-      <div className="h-1.5 bg-black w-24 mr-2 md:h-2 md:w-32  xl:w-60">&nbsp;</div>
+      <div className="h-1.5 bg-black w-24 mr-2 md:h-2 md:w-32 xl:w-60">&nbsp;</div>
       <div className="font-sans font-semibold tracking-wider text-sm leading-5 uppercase md:font-bold xl:text-lg">
         Sharing is Caring
       </div>
@@ -9,15 +9,27 @@ const Sharing = () => (
 
     <div className="flex flex-col pr-10 space-y-2 xs:flex-row xs:space-y-0 xs:space-x-3 md:pr-28 xl:pr-52">
       <a href="#" rel="noreferrer" target="_blank">
-        <img src="/icons/social/black/twitter-icon.svg" alt="Twitter" />
+        <img
+          src="/icons/social/black/twitter-icon.svg"
+          alt="Twitter"
+          className="h-8 md:h-9 xl:h-10"
+        />
       </a>
 
       <a href="#" rel="noreferrer" target="_blank">
-        <img src="/icons/social/black/linkedin-icon.svg" alt="Linkedin" />
+        <img
+          src="/icons/social/black/linkedin-icon.svg"
+          alt="Linkedin"
+          className="h-8 md:h-9 xl:h-10"
+        />
       </a>
 
       <a href="#" rel="noreferrer" target="_blank">
-        <img src="/icons/social/black/facebook-icon.svg" alt="Facebook" />
+        <img
+          src="/icons/social/black/facebook-icon.svg"
+          alt="Facebook"
+          className="h-8 md:h-9 xl:h-10"
+        />
       </a>
     </div>
   </div>

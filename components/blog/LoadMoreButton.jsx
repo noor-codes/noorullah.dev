94c5271a95c58b5fcf6bb2f23f4820887f6cdb76
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-11 01:37:46 +0430

    Button instead of <a> tag

--- components/blog/LoadMoreButton.jsx
+++ components/blog/LoadMoreButton.jsx
@@ -1,8 +1,11 @@
 const LoadMoreButton = () => (
   <div className="flex justify-center">
-    <a className="text-sm mt-14 text-gray-600 hover:bg-gray-200 active:bg-gray-200 cursor-pointer rounded-md px-4 py-2 transition duration-500 ease-out select-none">
+    <button
+      type="button"
+      className="text-sm mt-14 text-gray-600 hover:bg-gray-200 active:bg-gray-200 cursor-pointer rounded-md px-4 py-2 transition duration-500 ease-out select-none"
+    >
       Load More
-    </a>
+    </button>
   </div>
 )
 

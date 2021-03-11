5c7aa43ba25ace1863fce50ab483dbaf3f7e08b6
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-11 20:07:37 +0430

    lg breakpoint

--- components/blog/ArticleCard.jsx
+++ components/blog/ArticleCard.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 
 const ArticleCard = ({ title, paragraph }) => (
-  <div className="block pt-5 mx-8">
+  <div className="block pt-5 mx-8 lg:mb-2">
     <div className="flex">
       <img
         src="/img/profile.jpg"
         alt="topic"
-        className="h-16 w-16 rounded xs:h-20 xs:w-20 md:w-24 md:h-24"
+        className="h-16 w-16 rounded xs:h-20 xs:w-20 md:w-24 md:h-24 lg:h-40 lg:w-40"
       />
       <Link href="/blog/article">
         <a>
-          <div className="flex flex-col">
-            <h2 className="ml-3 leading-5 text-base font-semibold tracking-tight text-gray-700 xs:text-lg xs:leading-6 md:text-xl">
+          <div className="flex flex-col lg:justify-center h-full">
+            <h2 className="ml-3 lg:ml-8 leading-5 text-base font-semibold tracking-tight text-gray-700 xs:text-lg xs:leading-6 md:text-xl lg:text-2xl">
               {title}
             </h2>
-            <p className="text-xs pl-3 mt-1 text-gray-600 md:text-sm">{paragraph}</p>
+            <p className="text-xs lg:ml-5 pl-3 mt-1 text-gray-600 md:text-sm lg:text-lg">
+              {paragraph}
+            </p>
           </div>
         </a>
       </Link>
--- components/blog/BlogTitle.jsx
+++ components/blog/BlogTitle.jsx
@@ -1,11 +1,13 @@
 const BlogTitle = ({ title, description }) => (
   <>
     <div className="mt-8 mb-4 px-8">
-      <h1 className="font-bold leading-normal text-2xl tracking-tight xs:font-bold xs:text-3xl xs:leading-10 md:text-4xl lg:mb-6">
+      <h1 className="font-bold leading-normal text-2xl tracking-tight xs:font-bold xs:text-3xl xs:leading-10 md:text-4xl lg:mb-6 lg:pr-36 lg:leading-snug">
         {title}
       </h1>
     </div>
-    <p className="px-8 text-sm xs:text-base xs:tracking-tight md:text-lg">{description}</p>
+    <p className="px-8 text-sm xs:text-base xs:tracking-tight md:text-lg lg:pr-36 lg:text-xl">
+      {description}
+    </p>
   </>
 )
 

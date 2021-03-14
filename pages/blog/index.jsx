f06ffc688c65d3895b78a9265fd6df9944e6e131
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-14 17:42:37 +0430

    md breakpoint

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -5,53 +5,37 @@ import ArticleCard from '@/components/ArticleCard'
 const Blog = () => (
   <div>
     <PageName name="Blog" />
-    <div className="container min-w-xs mx-auto px-6">
+    <div className="container min-w-xs mx-auto px-6 sm:px-10">
       <main className="mx-auto md:pt-5">
         <div className="mt-8 mb-4">
-          <h1 className="font-bold leading-normal mb-5 text-3xl xs:text-4xl xs:leading-10 md:text-4xl lg:mb-6 lg:pr-36 lg:leading-snug lg:text-5xl">
+          <h1 className="font-bold leading-normal mb-5 text-3xl xs:text-4xl xs:leading-10 md:text-6xl md:mb-6 lg:mb-6 lg:leading-snug lg:text-5xl">
             All Blog Posts
           </h1>
-          <p className="text-gray-600 text-sm -mt-5 xs:text-base xs:-mt-3 sm:text-lg">
+          <p className="text-gray-700 text-sm -mt-5 xs:text-base xs:-mt-3 md:text-xl md:-mt-2  sm:text-lg">
             I love blogging about web development and the latest and great technologies that excites
             me.
           </p>
         </div>
-        <div className="mt-4 sm:mt-6">
-          <label
-            htmlFor="search"
-            className="relative mt-2 text-gray-500 lg:text-xl tracking-normal sm:text-lg"
-          >
-            Search Blog Posts
-            <img
-              src="/icons/search-icon.svg"
-              alt="Search Icon"
-              className="absolute h-5 lg:h-6 top-8 sm:top-9 lg:top-12 lg:text-xl right-3 cursor-pointer bg:gray-300"
-            />
-            <input
-              id="search"
-              type="text"
-              className="py-1 lg:py-2 mt-1 w-full border-2 flex-1 border-gray-300 rounded mb-4 focus:border-gray-400 text-gray-700 focus:ring-gray-400 lg:mt-2"
-            />
-          </label>
-        </div>
+
+        <div className="border-b-2 border-gray-100 -mt-5 md:-mt-2">&nbsp;</div>
 
         <ArticleCard
           title="Local Authentication Using passport in Node.JS"
-          paragraph="lorem ipsum dolor sit amet, consecteturlorem ipsum dolor sit amet consectetur adip lorem ipsum dolor sit amet consecteturlorem ipsum dolor sit amet, consectetur adip"
+          paragraph="lorem ipsum dolor sit amet, consecteturlorem ipsum dolor sit amet sit amet adip lorem ipsum dolor sit amet sit ametlorem ipsum dolor sit amet, sit amet, adip"
         />
         <ArticleCard
           title="New Features in Jest"
-          paragraph="lorem ipsum dolor sit amet, consecteturlorem ipsum dolor sit amet consectetur adip"
+          paragraph="lorem ipsum dolor sit amet, sit amet,lorem ipsum dolor sit amet sit amet adip"
         />
         <ArticleCard
           title="5 CSS Tricks you didn't know existed"
-          paragraph="lorem ipsum dolor sit amet, consecteturlorem ipsum dolor sit amet consectetur adip"
+          paragraph="lorem ipsum dolor sit amet, sit amet,lorem ipsum dolor sit amet sit amet adip"
         />
 
         <div className="flex justify-center">
           <button
             type="button"
-            className="text-sm mt-14 px-5 border-2 border-gray-200 text-gray-700 hover:bg-gray-900 hover:text-white cursor-pointer rounded-md py-2 transition duration-500 ease-out select-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black focus:ring-opacity-50 focus:ring-offset-white transform active:scale-90"
+            className="text-sm mt-14 px-5 border-2 select-none border-gray-400 hover:border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white cursor-pointer rounded-md py-2 transition duration-500 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black focus:ring-opacity-50 focus:ring-offset-white transform active:scale-90"
           >
             Load More
           </button>

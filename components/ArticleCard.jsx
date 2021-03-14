484c131cabccf67bf64f1ae6f1c0920aa17d8d9b
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-14 17:55:50 +0430

    lg breakpoint

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -1,15 +1,15 @@
 import Link from 'next/link'
 
 const ArticleCard = ({ title, paragraph }) => (
-  <div className="block pt-4 mb-2">
+  <div className="block pt-4 mb-2 lg:mb-4">
     <div className="flex">
       <Link href="/blog/article">
         <a>
-          <div className="flex flex-col lg:justify-center h-full md:mr-28">
+          <div className="flex flex-col lg:justify-center h-full">
             <h1 className="mb-2 leading-6 hover:text-blue-500 font-bold text-xl text-gray-900 xs:leading-6 xs:text-xl sm:text-2xl md:text-3xl md:mb-3 md:leading-10 xl:text-3xl xl:font-bold">
               {title}
             </h1>
-            <p className="text-sm text-gray-600 sm:text-base sm:leading-5 md:text-base md:-mt-1 md:tracking-tight lg:text-base line-clamp-2 sm:line-clamp-3">
+            <p className="text-sm lg:max-w-xl text-gray-600 sm:text-base sm:leading-5 md:text-base md:-mt-1 md:tracking-tight lg:text-base line-clamp-2 sm:line-clamp-3">
               {paragraph}
             </p>
           </div>
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -5,10 +5,10 @@ import ArticleCard from '@/components/ArticleCard'
 const Blog = () => (
   <div>
     <PageName name="Blog" />
-    <div className="container min-w-xs mx-auto px-6 sm:px-10">
+    <div className="container min-w-xs mx-auto px-6 sm:px-10 md:max-w-3xl">
       <main className="mx-auto md:pt-5">
         <div className="mt-8 mb-4">
-          <h1 className="font-bold leading-normal mb-5 text-3xl xs:text-4xl xs:leading-10 md:text-6xl md:mb-6 lg:mb-6 lg:leading-snug lg:text-5xl">
+          <h1 className="font-bold leading-normal mb-5 text-3xl xs:text-4xl xs:leading-10 md:text-6xl md:mb-6 lg:leading-snug">
             All Blog Posts
           </h1>
           <p className="text-gray-700 text-sm -mt-5 xs:text-base xs:-mt-3 md:text-xl md:-mt-2  sm:text-lg">
@@ -17,7 +17,7 @@ const Blog = () => (
           </p>
         </div>
 
-        <div className="border-b-2 border-gray-100 -mt-5 md:-mt-2">&nbsp;</div>
+        <div className="border-b-2 border-gray-100 -mt-5 md:-mt-4 lg:mb-8">&nbsp;</div>
 
         <ArticleCard
           title="Local Authentication Using passport in Node.JS"

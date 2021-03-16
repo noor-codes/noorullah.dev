b974faed72f2681fa24f1a12a31312b6590062a3
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-16 23:13:56 +0430

    NextJS Image Component && Links are more discoverable

--- components/Sharing.jsx
+++ components/Sharing.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const Sharing = () => (
   <div className="flex items-center justify-between mt-16 xl:pt-10">
     <div className="flex items-center">
@@ -8,28 +10,39 @@ const Sharing = () => (
     </div>
 
     <div className="flex flex-col pr-8 space-y-2 xs:flex-row xs:space-y-0 xs:space-x-3 md:pr-28 lg:pr-52 lg:space-x-4">
-      <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/black/twitter-icon.svg"
-          alt="Twitter"
-          className="h-8 md:h-9 lg:h-10"
-        />
+      <a href="#" rel="noreferrer" target="_blank" aria-label="twitter">
+        <div className="h-9 w-9 md:h-9 md:w-9 lg:h-10 lg:w-10">
+          <Image
+            src="/icons/social/black/twitter-icon.svg"
+            alt="Twitter"
+            className="h-8 md:h-9 lg:h-10"
+            height="300"
+            width="300"
+          />
+        </div>
       </a>
 
-      <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/black/linkedin-icon.svg"
-          alt="Linkedin"
-          className="h-8 md:h-9 lg:h-10"
-        />
+      <a href="#" rel="noreferrer" target="_blank" aria-label="linkedin">
+        <div className="h-9 w-9 md:h-9 md:w-9 lg:h-10 lg:w-10">
+          <Image
+            src="/icons/social/black/linkedin-icon.svg"
+            alt="Linkedin"
+            className="h-8 md:h-9 lg:h-10"
+            height="300"
+            width="300"
+          />
+        </div>
       </a>
 
-      <a href="#" rel="noreferrer" target="_blank">
-        <img
-          src="/icons/social/black/facebook-icon.svg"
-          alt="Facebook"
-          className="h-8 md:h-9 lg:h-10"
-        />
+      <a href="#" rel="noreferrer" target="_blank" aria-label="facebook">
+        <div className="h-9 w-9 md:h-9 md:w-9 lg:h-10 lg:w-10">
+          <Image
+            src="/icons/social/black/facebook-icon.svg"
+            alt="Facebook"
+            height="300"
+            width="300"
+          />
+        </div>
       </a>
     </div>
   </div>

2acf94e46a5df8fe6279bda4571d15432816ca9b
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-03-16 12:53:35 +0430

    mintues of read

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 
 const ArticleCard = ({ title, paragraph }) => (
-  <div className="block pt-4 mb-2 lg:mb-4">
+  <div className="block pt-4 mb-5 lg:mb-8">
     <div className="flex">
       <Link href="/blog/article">
         <a>
           <div className="flex flex-col lg:justify-center h-full">
-            <h1 className="mb-2 leading-6 hover:text-blue-500 font-bold text-xl text-gray-900 xs:leading-6 xs:text-xl sm:text-2xl md:text-3xl md:mb-3 md:leading-10 xl:text-3xl xl:font-bold">
+            <h1 className="mb-2 leading-6 hover:text-blue-500 font-bold text-xl text-gray-900 xs:leading-6 max-w-xl xs:text-xl sm:text-2xl md:text-3xl  md:mb-3 md:leading-10 xl:text-3xl xl:font-bold">
               {title}
             </h1>
             <p className="text-sm lg:max-w-xl text-gray-600 sm:text-base sm:leading-5 md:text-base md:-mt-1 md:tracking-tight lg:text-base line-clamp-2 sm:line-clamp-3">
@@ -16,6 +16,9 @@ const ArticleCard = ({ title, paragraph }) => (
         </a>
       </Link>
     </div>
+    <span className="block text-xs text-gray-400 mt-2 xs:text-sm lg:text-base">
+      10 mins of read
+    </span>
   </div>
 )
 

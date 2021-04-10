6c740ba42c7e31d98d76306e3643973e1d9ccb71
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-11 01:44:50 +0430

    Navigation Integrated with Index Page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Layout from '@/components/Layout'
 import Head from 'next/head'
 
 export default function Home() {
@@ -7,11 +8,13 @@ export default function Home() {
         <title>Noorullah Ahmadzai</title>
       </Head>
 
-      <main className="flex flex-col justify-center text-center h-screen ">
-        <h1 className="text-7xl font-bold mb-2">Noorullah Ahmadzai</h1>
+      <Layout>
+        <main className="flex flex-col justify-center text-center h-screen ">
+          <h1 className="text-7xl font-bold mb-2">Noorullah Ahmadzai</h1>
 
-        <p className="text-2xl">Website is currently under construction.</p>
-      </main>
+          <p className="text-2xl">Website is currently under construction.</p>
+        </main>
+      </Layout>
     </div>
   )
 }

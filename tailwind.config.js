ddad302dd43b54eabeb39e72cba83c41202af496
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-11 00:55:31 +0430

    Latest Tailwind Jit Compiler in Config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {

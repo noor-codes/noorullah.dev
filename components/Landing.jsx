d0bcdcff75dc29b1bea84e719ea2fa303028484e
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-11 19:50:18 +0430

    Spacing Tweaks

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -31,7 +31,7 @@ const Landing = () => {
             </h2>
           </div>
 
-          <div className="flex justify-center my-8">
+          <div className="flex justify-center mt-6 mb-2">
             <button
               type="button"
               className="flex items-center justify-center w-full py-2 bg-gray-900 text-white  rounded hover:bg-gray-700 cursor-pointer transition duration-500 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black focus:ring-opacity-50 focus:ring-offset-white transform active:scale-90"

6202b5866afeabce68fb3943edc3dab7c7b3c3a4
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-12 18:49:12 +0430

    md breakpoint

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
   return (
     <>
       <div className="container min-w-xs mx-auto px-6 sm:px-10 md:max-w-2xl xl:max-w-3xl">
-        <main className="flex flex-col justify-center pt-10 sm:mt-16">
+        <main className="flex flex-col justify-center pt-10 sm:mt-16 md:mt-24">
           <div className="flex items-center mb-4">
             <div className="h-14 w-14 xs:w-16 xs:h-16">
               <Image
@@ -143,8 +143,16 @@ const Landing = () => {
         </div>
       </div>
 
-      <div className="bg-gray-900 text-white py-3 pl-6 font-semibold xs:text-lg sm:text-xl sm:py-4 sm:pl-20">
-        Need a full stack developer?
+      <div className="bg-gray-900 text-white py-3 pl-6 font-semibold xs:text-lg sm:text-xl sm:py-4 lg:text-2xl lg:py-6">
+        <div className="flex items-center justify-between container min-w-xs mx-auto sm:px-4 md:max-w-2xl xl:max-w-3xl">
+          Need a full stack developer?
+          <button
+            type="button"
+            className="sm:flex hidden items-center text-sm px-6 mr-8 border-2 select-none border-gray-400 hover:border-gray-900 text-white hover:bg-gray-100 hover:text-gray-900 cursor-pointer rounded-md py-2 transition duration-500 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white focus:ring-opacity-50 focus:ring-offset-gray-900 transform active:scale-95"
+          >
+            Shoot me a message
+          </button>
+        </div>
       </div>
 
       <div className="container min-w-xs mx-auto px-6 xs:px-10 sm:max-w-lg md:max-w-2xl xl:max-w-3xl">
@@ -202,7 +210,11 @@ const Landing = () => {
               </div>
               <div className="flex justify-center sm:mt-4">
                 <button
-                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault()
+                  }}
+                  name="submit"
+                  type="submit"
                   className="flex items-center text-sm px-6 border-2 select-none border-gray-400 hover:border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white cursor-pointer rounded-md py-2 transition duration-500 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black focus:ring-opacity-50 focus:ring-offset-white transform active:scale-95 disabled:bg-gray-200 disabled:hover:text-gray-700 disabled:hover:border-gray-400 disabled:cursor-not-allowed"
                 >
                   Submit

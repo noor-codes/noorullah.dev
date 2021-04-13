4a066e4b76ce78ab151701552354a15d98dd1f96
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-13 20:43:54 +0430

    More Optimization into Lighthouse Score, Removing Bugs

--- pages/_document.js
+++ pages/_document.js
@@ -12,11 +12,11 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            rel="preload"
+            rel="stylesheet preloade"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             type="font/woff2"
             as="font"
-            crossOrigin
+            crossOrigin="anonymous"
           />
         </Head>
         <body>

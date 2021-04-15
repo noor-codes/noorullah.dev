b00e1e5615940feb00cf9086c02f9be9c35c7be8
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-15 22:41:57 +0430

    disabled pwa in development

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
   },
   webpack(config) {
     config.module.rules.push({

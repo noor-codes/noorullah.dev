4db874c8cc63f7a48b743b73ab1180f8dc384a54
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-17 01:10:15 +0430

    next-pwa to next-offline for PWA support

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// NEXT-OFFLINE PWA
+const withOffline = require('next-offline')
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
-  },
-  future: {
-    webpack5: true,
-  },
+module.exports = withOffline({
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -17,6 +9,29 @@ module.exports = withPWA({
     })
 
     return config
+  },
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ]
   },
 })
 
@@ -30,3 +45,25 @@ module.exports = withPWA({
 //     return config
 //   },
 // }
+
+// const withPWA = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     runtimeCaching,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+//   future: {
+//     webpack5: true,
+//   },
+// webpack(config) {
+//   config.module.rules.push({
+//     test: /\.svg$/,
+//     use: ['@svgr/webpack'],
+//   })
+
+//   return config
+// },
+// })

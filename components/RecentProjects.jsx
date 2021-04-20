f4a9625b679e91095f5cd73d96ce5f2821d5bc20
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-20 19:59:22 +0430

    recent projects card size reduced

--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -7,22 +7,22 @@ const RecentProjects = () => {
     <>
       <div className="flex flex-wrap gap-5 justify-center xs:justify-start text-center mt-8 mb-10">
         <div>
-          <div className="flex justify-center items-center bg-gray-100 h-36 w-36 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
-            <Inventory className="h-20 w-h-20 fill-current" />
+          <div className="flex justify-center items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+            <Inventory className="h-16 w-h-16 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2 ">Inventory</div>
         </div>
 
         <div>
-          <div className="flex justify-center items-center bg-gray-100 h-36 w-36 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
-            <Portfolio className="h-20 w-h-20 fill-current" />
+          <div className="flex justify-center items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+            <Portfolio className="h-16 w-h-16 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2">My Portfolio</div>
         </div>
 
         <div>
-          <div className="flex justify-center items-center bg-gray-100 h-36 w-36 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
-            <Carbidnow className="h-24 w-h-24 fill-current" />
+          <div className="flex justify-center items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+            <Carbidnow className="h-20 w-h-20 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2">CarBidNow</div>
         </div>

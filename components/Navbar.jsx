b5957df2c7ae7f6332f7a808c315aa1393561fd7
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-20 13:20:41 +0430

    Dark Mode for Responsive Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,6 @@ import { useTheme } from 'next-themes'
 import CodeIcon from '@/public/icons/menu/code-icon.svg'
 import BurgerIcon from '@/public/icons/menu/burger-icon.svg'
 import CloseMenuIcon from '@/public/icons/menu/closemenu-icon.svg'
-import MoonIcon from '@/public/icons/menu/moon-icon.svg'
-import SunIcon from '@/public/icons/menu/sun-icon.svg'
 import BlogIcon from '@/public/icons/menu/blog-icon.svg'
 import TwitterIcon from '@/public/icons/social/twitter-icon.svg'
 
@@ -16,9 +14,6 @@ const Navbar = () => {
 
   //. theme is accessed after it is mounted
   useEffect(() => setMounted(true), [])
-
-  //? Dark Mode Toggle
-  const [isDark, setIsDark] = useState(false)
 
   const [isOpen, setIsOpen] = useState(false)
   const menuVisibility = isOpen ? '' : 'sr-only' // sr-only instead of hidden class for accessibility
@@ -133,33 +128,38 @@ const Navbar = () => {
             </a>
 
             <button type="button" aria-label="dark mode" className="focus:outline-none">
-              {isDark ? (
-                <a
-                  href="#"
-                  className="flex items-center pr-1 text-sm hover:bg-gray-800 dark:hover:bg-white dark:hover:text-gray-900 px-2 py-2 rounded"
-                  onClick={() => {
-                    setIsDark(!isDark)
-                    setIsOpen(!isOpen)
-                    setTheme('dark')
-                  }}
-                >
-                  <MoonIcon className="inline h-6 w-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8 fill-current hover:opacity-50 mr-2" />
-                  Dark Mode
-                </a>
-              ) : (
-                <a
-                  href="#"
-                  onClick={() => {
-                    setIsDark(!isDark)
-                    setIsOpen(!isOpen)
-                    setTheme('light')
-                  }}
-                  className="flex items-center pr-1 text-sm hover:bg-gray-800 dark:hover:bg-white dark:hover:text-gray-900 px-2 py-2 rounded"
-                >
-                  <SunIcon className="inline h-7 w-7 sm:w-7 sm:h-7 lg:h-8 lg:w-8 fill-current hover:opacity-50 mr-1" />
-                  Light Mode
-                </a>
-              )}
+              <a
+                href="#"
+                className="flex items-center pr-1 text-sm hover:bg-gray-800 dark:hover:bg-white dark:hover:text-gray-900 px-2 py-2 rounded"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              >
+                {mounted && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    stroke="currentColor"
+                    className="h-6 w-6 dark:text-gray-800 text-gray-200"
+                  >
+                    {theme === 'dark' ? (
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                      />
+                    ) : (
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1}
+                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                      />
+                    )}
+                  </svg>
+                )}
+                &nbsp; {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+              </a>
             </button>
           </div>
         </div>

bddfcb427ddfe083102c5b2693aef3cc95da2ddb
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-20 20:02:17 +0430

    tech stack icon size changed

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -7,7 +7,7 @@ const TechStack = () => {
         return (
           <div key={tech.name}>
             <div className="flex justify-center items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
-              <tech.icon className="h-20 w-20 fill-current" />
+              <tech.icon className="h-16 w-16 fill-current" />
             </div>
             <div className="text-sm text-gray-500 mt-2">{tech.name}</div>
           </div>

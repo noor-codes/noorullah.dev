d3827bbd4a37842f4a6d619ac0682216154641f5
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-22 14:46:35 +0430

    Tech Stack and Work Experience Grid Flex Gap Issue Resolved

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -2,11 +2,11 @@ import { details } from '@/components/icons/landingIcons'
 
 const TechStack = () => {
   return (
-    <div className="flex flex-wrap gap-4 justify-center xs:justify-start text-center mt-8">
+    <div className="flex flex-wrap -mx-4 justify-center xs:justify-start text-center mt-8">
       {details.map((tech) => {
         return (
           <div key={tech.name}>
-            <div className="flex justify-center items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
+            <div className="flex justify-center m-4 items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
               <tech.icon className="h-16 w-16 fill-current" />
             </div>
             <div className="text-sm text-gray-500 mt-2">{tech.name}</div>

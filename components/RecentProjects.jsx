33b383232c64bd3b0d6297ed0990d7fc82b5e1d8
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-22 17:20:54 +0430

    minor changes

--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -5,23 +5,23 @@ import Carbidnow from '@/public/icons/landing/projects/carbidnow-icon.svg'
 const RecentProjects = () => {
   return (
     <>
-      <div className="flex flex-wrap -mx-5 justify-center xs:justify-start text-center mt-8 mb-10">
+      <div className="flex flex-wrap -mx-3 justify-center xs:justify-start text-center mt-8 mb-10">
         <div>
-          <div className="flex justify-center items-center m-5 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+          <div className="flex justify-center items-center m-3 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
             <Inventory className="h-16 w-h-16 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2 ">Inventory</div>
         </div>
 
         <div>
-          <div className="flex justify-center items-center m-5 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+          <div className="flex justify-center items-center m-3 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
             <Portfolio className="h-16 w-h-16 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2">My Portfolio</div>
         </div>
 
         <div>
-          <div className="flex justify-center items-center m-5 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
+          <div className="flex justify-center items-center m-3 bg-gray-100 h-28 w-28 text-2xl text-gray-600 rounded-md dark:bg-gray-800 dark: dark:text-gray-400 ">
             <Carbidnow className="h-20 w-h-20 fill-current" />
           </div>
           <div className="text-sm text-gray-500 mt-2">CarBidNow</div>
--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -1,12 +1,12 @@
-import { details } from '@/components/icons/landingIcons'
+import { details } from '@/components/icons/TechStackIcons'
 
 const TechStack = () => {
   return (
-    <div className="flex flex-wrap -mx-4 justify-center xs:justify-start text-center mt-8">
+    <div className="flex flex-wrap -mx-3 justify-center xs:justify-start text-center mt-8">
       {details.map((tech) => {
         return (
           <div key={tech.name}>
-            <div className="flex justify-center m-4 items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
+            <div className="flex justify-center m-3 items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
               <tech.icon className="h-16 w-16 fill-current" />
             </div>
             <div className="text-sm text-gray-500 mt-2">{tech.name}</div>

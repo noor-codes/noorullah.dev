410fb4902fac9ead503bdd7247a88477efec74ea
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-22 16:27:53 +0430

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import GitHub from '@/public/icons/social/github-icon.svg'
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center bg-gray-900 dark:bg-white gap-y-2 w-full h-44 mt-20 md:h-60 xl:h-60">
+    <footer className="flex flex-col justify-center bg-gray-900 dark:bg-white gap-y-2 w-full h-44 mt-20 md:h-60 xl:h-60">
       <p className="text-white text-3xl mx-10 text-center md:mb-1 xl:text-4xl xl:mb-2 dark:text-gray-900">
         Say &ldquo;Hi&ldquo;
       </p>
@@ -39,7 +39,7 @@ const Footer = () => {
           <LinkedIn className="fill-current hover:opacity-50 text-white dark:text-gray-900" />
         </a>
       </div>
-    </div>
+    </footer>
   )
 }
 export default Footer

ceafaa5ffdbd92860b7831736448c42edd729e82
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-25 01:19:28 +0430

    Accessibility Issues Resolved

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -9,7 +9,7 @@ const TechStack = () => {
             <div className="flex justify-center m-3 items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
               <tech.icon className="h-16 w-16 fill-current" />
             </div>
-            <div className="text-sm text-gray-400 mt-2">{tech.name}</div>
+            <div className="text-sm text-gray-500 mt-2">{tech.name}</div>
           </div>
         )
       })}

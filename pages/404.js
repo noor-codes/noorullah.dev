19c0a651e792d049b45a08813b21ff897414504a
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-25 18:31:07 +0430

    404 Margin Bottom

--- pages/404.js
+++ pages/404.js
@@ -38,7 +38,7 @@ const NotFound = () => {
               The page could not be found!
             </h2>
 
-            <p className="text-sm mt-2 text-gray-500 dark:text-gray-500 md:text-base lg:text-lg">
+            <p className="text-sm mt-2 text-gray-500 dark:text-gray-500 md:text-base md:mb-20 lg:text-lg">
               The page you are looking for does not exist, have been removed, name changed or is
               temporarily unavailable.
             </p>

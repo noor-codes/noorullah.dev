8e7f47233a020d11c4da05c08eb075ed18b1d958
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-26 23:45:14 +0430

    Blog SEO

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -13,13 +13,13 @@ const Blog = ({ posts }) => {
           type: 'blog',
           images: [
             {
-              url: 'http://noorullah.dev',
+              url: `${process.env.NEXT_PUBLIC_ASSETS_URL}/img/SEO/Blog.png`,
               width: 800,
               height: 600,
               alt: 'Noorullah Ahmadzai',
             },
 
-            { url: 'http://noorullah.dev' },
+            { url: `${process.env.NEXT_PUBLIC_ASSETS_URL}/img/SEO/Blog.png` },
           ],
         }}
       />

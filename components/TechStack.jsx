804392c59f4d4ef776a3ee6c768bcae7190acbf2
Noorullah-Ahmadzai <noorullah.ahmadzai76@gmail.com>
2021-04-28 21:14:57 +0430

    Tech Stack and Recent Projects more organized

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -6,8 +6,8 @@ const TechStack = () => {
       {details.map((tech) => {
         return (
           <div key={tech.name}>
-            <div className="flex justify-center m-3 items-center bg-gray-100 h-28 w-28 text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
-              <tech.icon className="h-16 w-16 fill-current" />
+            <div className="flex justify-center m-3 items-center bg-gray-100 h-24 w-24 sm:h-28 sm:w-28  text-2xl text-gray-600 dark:bg-gray-800 dark: dark:text-gray-400 rounded-md">
+              <tech.icon className="h-14 w-14 sm:h-16 sm:w-16 fill-current" />
             </div>
             <div className="text-sm text-gray-500 mt-2">{tech.name}</div>
           </div>

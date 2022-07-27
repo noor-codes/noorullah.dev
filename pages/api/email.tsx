66a7b2fbda9f765c15d25cb5690a9171b42b215e
Noorullah Ahmadzai <Noorullah.Ahmadzai76@gmail.com>
2022-07-28 02:00:05 +0430

    simple email

--- pages/api/email.tsx
+++ pages/api/email.tsx
@@ -23,68 +23,7 @@ const Email = async (req, res) => {
         to: process.env.EMAIL,
         from: 'noorullah.ahmadzai76@gmail.com',
         subject: req.body.name,
-        html: `
-        <html>
-          <body style="margin:0;padding:0;">
-              <div style="background:#fff">
-                  <div style="max-width:100%;margin:0px auto;">
-                      <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%; background-color:#fff;">
-                          <tbody>
-                              <tr>
-                                  <td>
-                                      <div style="max-width:100%;box-sizing:border-box; background:#161616">
-                                          <div style="width:100%;max-width:575px;min-width:300px;background:#fff;margin:auto;box-sizing:border-box;border-radius:4px;border-bottom-left-radius:0;border-bottom-right-radius:0;padding:50px 30px 10px;">
-                                              <h1 style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';color:#3d4852;font-size:18px;font-weight:bold;margin-top:0;text-align:left">
-                                                ${req.body.name}
-                                              </h1>
-                                              <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
-                                                ${req.body.msg}
-                                              </p>
-                                          </div>
-                                      </div>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td>
-                                      <div style="width:100%;max-width:575px;min-width:300px;margin-left:auto;margin-right:auto; box-sizing:border-box;border-radius:4px;border-top-left-radius:0;border-top-right-radius:0;padding:10px 30px 50px; box-shadow: 5px 5px 5px #dadada;">
-
-                                          <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
-                                            From: ${req.body.email}<br>
-                                          </p>
-                                      </div>
-                                  </td>
-                              </tr>
-                          </tbody>
-                      </table>
-                  </div>
-
-                  <div style="max-width:100%;margin:0px auto;">
-                      <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%">
-                          <tbody>
-                              <tr>
-                                  <td>
-                                      <div style="width:100%;max-width:575px;min-width:300px;margin:auto;box-sizing:border-box;padding-top:20px;padding-bottom:20px;padding-left:15px;padding-right:15px;">
-                                          <p style="text-align:center; font-family:verdana;">
-                                              <p style="text-align:center;font-size:13px;line-height:1.5;color:#999999; text-decoration: none; color: cornflowerblue;     display: flex; align-items: center; justify-content: center;">
-                                            </p>
-                                          </p>
-
-                                          <p style="text-align:center; font-family:verdana;">
-                                            <a href="http://noorullah.dev/" style="text-align:center;font-size:14px;line-height:1.5;color:#999999; text-decoration: none; color: cornflowerblue;     display: flex; align-items: center; justify-content: center;">
-                                              Noorullah Ahmadzai
-                                            </a>
-                                          </p>
-
-                                      </div>
-                                  </td>
-                              </tr>
-                          </tbody>
-                      </table>
-                  </div>
-              </div>
-          </body>
-        </html>
-        `,
+        html: ` ${req.body.name} <br/> ${req.body.msg} <br/> from: ${req.body.email}`,
       })
 
       return res.json({ msg: 'Received! Thank You! 😍' })

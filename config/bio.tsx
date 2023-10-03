c64c8d99efcc776c182d6f1bbef49ab540033b70
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-03 19:39:38 +0430

    better message

--- config/bio.tsx
+++ config/bio.tsx
@@ -9,8 +9,7 @@ export const bio: bio_types = {
   cv_url: '/cv',
 
   about_author: `Noorullah Ahmadzai is a Software Engineer, Web Developer, Writer and Bachelor of
-  Computer Science. He is passionate about building quality software that everyone
-  loves.`,
+  Computer Science. He is passionate about building quality software.`,
 
   landing_bio: `A software engineer, web developer, writer and bachelor of computer science. I have experience in a wide variety of programming languages and software development tools. I am passionate about developing software that is reliable, efficient, and user-friendly. I am always looking to improve my skills and learn new technologies.`,
   blog_intro: `Web Technologies, JAMStack, General Information. If you ask me a question twice. I will blog about it.`,

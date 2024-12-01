e0f9ab8dc98a1ef4263623e0fd74b9086c149511
Noorullah Ahmadzai <Noorullah.Ahmadzai76@gmail.com>
2024-12-02 00:48:10 +0430

    framer motion upgraded

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
+    domains: ['localhost'],
+    unoptimized: true
   },
   // Enable React strict mode for better performance
   reactStrictMode: true,

43b28cb85c5731bb6a1f05f1c7975b7409fbf2f0
Noorullah Ahmadzai <Noorullah.Ahmadzai76@gmail.com>
2024-12-01 22:36:04 +0430

    Tailwind Upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,37 @@
-/* eslint-disable global-require */
-const defaultTheme = require('tailwindcss/defaultTheme')
+import { screens } from 'tailwindcss/defaultTheme'
 
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    screens: {
-      xs: '480px',
-      ...defaultTheme.screens,
-    },
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#333',
-            a: {
-              color: '#3b82f6',
-              '&:hover': {
-                color: '#60a5fa',
-              },
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+  './layouts/**/*.{js,ts,jsx,tsx}',
+]
+export const theme = {
+  screens: {
+    xs: '480px',
+    ...screens,
+  },
+  extend: {
+    typography: {
+      DEFAULT: {
+        css: {
+          color: '#333',
+          a: {
+            color: '#3b82f6',
+            '&:hover': {
+              color: '#60a5fa',
             },
           },
         },
       },
-      fontFamily: {
-        sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
-      },
-      fontSize: {
-        tiny: '0.66rem',
-        bigger: '0.80rem',
-      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    fontSize: {
+      tiny: '0.66rem',
+      bigger: '0.80rem',
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
 }
+export const plugins = [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
